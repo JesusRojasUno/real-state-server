36587fe5549bb92c442225d742f41a8bd8bfe83f
Jesus Rojas <jesusrojasgz@gmail.com>
2022-10-20 18:11:52 -0600

    guardar cambios

---
+++ config.js
@@ -0,0 +1,11 @@
+const configuration = {
+
+mongodb:{
+    URL: process.env.DB_URL || "mongodb+srv://root:ULSA@cluster0.v7xgf.mongodb.net/test"
+    
+}
+
+    }
+    module.exports={
+        configuration
+}
