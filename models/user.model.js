e14f5449d665d8ac62373d21a9fd8ddeae4c8638
Jesus Rojas <jesusrojasgz@gmail.com>
2022-10-20 18:15:02 -0600

    guardado?

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true   
+    },
+    LastName:{
+        type: String,
+        required: true
+    },
+    UserName:{
+        type: String,
+        required: true
+    },
+    Password:{
+        type: String,
+        required: true
+    },
+    ProfileImage:{
+        type: String,
+        required: false
+
+        },
+});
+ 
+const User = mongoose.model('User',UserSchema);
+
+module.exports = {
+    User
+
+}
